7c41859bc8d8c02f2e33626e3b36bb7f2889c8be
lincherry <lincherryclf@163.com>
2019-02-28 15:15:17 +0800

    feat: AR模块更新
    1 在Play Settings中增加Vuforia AR Support，修改Package Name， Android TV Compatibility
    2 导入Vuforia包 ，增加文件夹Editor-Vuforia，  Resources，  Vuforia
    3 添加Scenes-ARTest场景
    4 Building Settings 中切换Android 平台
    5 导入ImageTarget数据库，增加文件夹Editor-Vuforia-ImageTargetTextures，StreamingAssets-Vuforia
    6 增加文件夹Materials-AR，Audios

---
+++ Assets/Scrpits/AR/WarFieldController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarFieldController : MonoBehaviour
+{
+    public GameObject BGRFX = null;
+
+    public float RiseSpeed = 1f;
+
+    public float MaxRotateAngle = 180f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Mathf.Abs(transform.localPosition.y - 0.1f) < 0.01f)
+        {
+            if(BGRFX != null)
+                Destroy(BGRFX);
+            return;
+        }  
+        transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(0, 0.1f, 0), RiseSpeed * Time.deltaTime);
+	}
+
+    public void RotateWarField(float factor)
+    {
+        transform.localEulerAngles  = new Vector3(0, factor * MaxRotateAngle, 0);
+    }
+
+    public void ScaleWarField(float factor)
+    {
+        transform.localScale = new Vector3(5, 0.2f, 3) * (factor + 1);
+    }
+}
