91b6663e7ef63e1b7b96cb1341bd23eb56db6098
L-Kr <987748679@qq.com>
2019-03-22 10:04:52 +0800

    新增布阵相关的常量

--- Assets/RealFram/BaseFramework/ConStr.cs
+++ Assets/RealFram/BaseFramework/ConStr.cs
@@ -13,6 +13,8 @@ public class ConStr {
     public const string EXITPANEL = "ExitPanel";
     public const string SETTINGPANEL = "SettingPanel";
     public const string ALERTPANEL = "AlertPanel";
+    public const string EMBATTLEPANEL = "EmbattlePanel";
+    public const string SELECTPANEL = "SelectPanel";
     //场景名称
     public const string EMPTYSCENE = "Empty";
     public const string MENUSCENE = "Menu";
@@ -20,4 +22,18 @@ public class ConStr {
 
     //预加载资源
     public const string ALERTPRE = "Assets/GameData/Prefabs/UGUI/Panel/AlertPanel.prefab";
+
+    //小兵编号
+    public const byte ArmsNull = 48;
+    public const byte ArmsCavalry = 49;
+    public const byte ArmsBowmen = 50;
+    public const byte ArmsMauler = 51;
+    public const byte ArmsInfantry = 52;
+
+    public const int CostNull = 0;
+    public const int CostCavalry = 1;
+    public const int CostBowmen = 2;
+    public const int CostMauler = 3;
+    public const int CostInfantry = 4;
+
 }
